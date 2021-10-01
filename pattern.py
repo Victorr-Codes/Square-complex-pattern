543a3fe66dc6fb73f86a0587038bd4d03afc3d3e
Victorr-Codes <72153622+Victorr-Codes@users.noreply.github.com>
2021-10-01 10:06:21 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,19 @@
+import turtle
+t = turtle.Turtle()
+
+s = turtle.Screen()
+s.bgcolor("black")
+c = 0
+d = 0
+while 1:
+    for i in range(4):
+        t.forward(80)
+        t.right(90)
+    t.right(30)
+    c += 1
+    if c >= 390 / 30:
+        t.forward(50)
+        c = 0
+        d += 1
+        if d >= 12:
+            continue
